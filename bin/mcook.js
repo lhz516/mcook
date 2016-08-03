06dd0c92f624e6e624e169b9f2cfd9f432e6f3c0
Hanz Luo <lhz0516@hotmail.com>
2016-08-02 19:58:22 -0700

    generate files in .meteor directory

--- bin/mcook.js
+++ bin/mcook.js
@@ -1,13 +1,49 @@
 #!/usr/bin/env node
+'use strict';
 
-var program = require('commander');
+const program = require('commander');
 
 program
-  .version('0.0.1', '-v, --version')
+  .version('0.0.1', '-v, --version');
+
+program
+  .usage('[command] [options]')
   .command('init [config]')
   .description('initialize a project')
-  .action(function (file) {
-    console.log(file);
+  .action((file) => {
+    let configFile = 'mcook.json';
+    if (file) {
+      configFile = file;
+    }
+    const fs = require('fs');
+    fs.readFile(configFile, 'utf8', function (err, data) {
+      if (err) {
+        console.log('Read configuration file failed');
+      } else {
+        console.log('Read configuration file success');
+        let obj;
+        try {
+          obj = JSON.parse(data);
+        } catch (err) {
+          console.log('Json parsing failed, please check the file format');
+        }
+        if (obj) {
+          const Mcook = require('../lib');
+          const mcook = new Mcook(obj);
+          mcook.validateConfiguration((err) => {
+            if (err) {
+              console.log(err.message);
+            } else {
+              mcook.createProject((err) => {
+                if (err) {
+                  console.log(err.message);
+                }
+              });
+            }
+          });
+        }
+      }
+    });
   });
 
 program
@@ -17,3 +53,7 @@ program
   });
 
 program.parse(process.argv);
+
+if (!process.argv[2]) {
+  console.log(program.help());
+}

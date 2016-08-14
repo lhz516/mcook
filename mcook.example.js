214984c17b289466ea57a1c3c3f25f9edcf9ef3c
Hanz Luo <lhz0516@hotmail.com>
2016-08-13 23:35:02 -0700

    output components files

--- mcook.example.js
+++ mcook.example.js
@@ -3,14 +3,85 @@ module.exports = {
   meteorCore: {
     version: '1.4.0.1'
   },
+  npm: {
+    packages: [
+      {
+        name: 'react',
+        version: '15.3.0'
+      }
+    ]
+  },
   collections: [
     {
-      name: 'Lists',
-      dbName: 'lists'
+      name: 'Posts',
+      dbName: 'posts'
     },
     {
       name: 'Comments',
       dbName: 'comments'
     }
-  ]
+  ],
+  redux: {
+    actions: [],
+    reducers: []
+  },
+  router: {
+    routes: [
+      {
+        name: 'layout',
+        component: 'MainLayout',
+        subComponents: [
+          {
+            name: 'navigation',
+            component: 'Navigation'
+          },
+          {
+            name: 'footer',
+            component: 'Footer'
+          }
+        ],
+        //wrapChildRoutes: true,
+        childRoutes: [
+          {
+            name: 'home',
+            path: '/',
+            component: 'Home',
+          },
+          {
+            name: 'posts',
+            path: '/posts',
+            component: 'Posts',
+            container: {
+              tracker: [
+
+              ],
+              state: [
+
+              ],
+              dispatch: [
+
+              ]
+            },
+            // childRoutes: [
+            //   {
+            //     isIndex: true,
+            //     component: 'PostIndex'
+            //   },
+            //   {
+            //     path: '/posts/:id',
+            //     component: 'Post'
+            //   }
+            // ],
+            wrapChildRoutes: true
+          }
+        ]
+      }
+    ],
+    redirects: [
+      {
+        from: '',
+        to: ''
+      }
+    ]
+  }
 };
